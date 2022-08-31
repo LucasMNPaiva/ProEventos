07f703b8ed078e2a6ab7568d1caf7868036036b1
LucasMNPaiva <lucas_nascimento@outlook.com>
2022-08-30 22:07:58 -0300

    First Commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento()
+                {
+                EventoId =1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.ToString()
+                },
+                new Evento()
+                {
+                EventoId =1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.ToString()
+                }
+            };
+
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+    }
+}
