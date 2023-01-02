19e6d8d6f9b3d4f95ab710fba595882e7e834ec9
LucasMNPaiva <lucas_nascimento@outlook.com>
2023-01-02 17:26:01 -0300

    Two way data bindings

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -4,14 +4,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
 
   public eventos : any = [];
+  public listaEventosFiltrada : any = [];
   exibirImagem = true;
   margemImagem =2
-  larguraImagem = 50
+  larguraImagem = 150
+  private _filtroLista='';
+
+  public get filtroLista() : string {
+    return this._filtroLista;
+  }
+  public set filtroLista(eventos : string) {
+    this._filtroLista = eventos;
+    this.listaEventosFiltrada = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor : string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
@@ -19,7 +37,10 @@ export class EventosComponent implements OnInit {
   }
   public getEventos():void{
     this.httpClient.get('https://localhost:5001/api/eventos').subscribe(
-      response=> this.eventos = response,
+      response=>{
+        this.eventos = response,
+        this.listaEventosFiltrada = this.eventos
+      },
       error => console.log(error),
     );
   }
